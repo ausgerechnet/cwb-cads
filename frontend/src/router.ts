c9b303c669d3ddc02a0e5a318372edff1ed9a6b2
flomei <contact@florianmeinicke.com>
2024-02-07 11:32:43 +0100

    Add default error component

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,14 +1,15 @@
 import { createRouter } from '@tanstack/react-router'
 import { queryClient } from '@/rest-client/client'
+import { DefaultErrorComponent } from '@/components/default-error-component'
 import { routeTree } from './routeTree.gen'
 
 export const router = createRouter({
   routeTree,
   defaultStaleTime: 0,
+  defaultPreloadStaleTime: 3_000,
   defaultPreload: 'intent',
-  context: {
-    queryClient,
-  },
+  defaultErrorComponent: DefaultErrorComponent,
+  context: { queryClient },
 })
 
 declare module '@tanstack/react-router' {
