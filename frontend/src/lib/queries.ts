7eb76ed9ce07c340a6fbac02e1aa2f69a891577f
flomei <contact@florianmeinicke.com>
2024-02-17 09:08:15 +0100

    Snappier UI response to logout

--- frontend/src/lib/queries.ts
+++ frontend/src/lib/queries.ts
@@ -179,6 +179,7 @@ export const logoutMutationOptions: MutationOptions = {
     localStorage.removeItem('access-token')
   },
   onSuccess: () => {
+    queryClient.setQueryData(sessionQueryOptions.queryKey, null)
     void queryClient.invalidateQueries(sessionQueryOptions)
   },
 }
