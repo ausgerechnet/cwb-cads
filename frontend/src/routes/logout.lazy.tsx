6cf32f84703c54cfbae8490ad2979d1dc8962f7c
flomei <contact@florianmeinicke.com>
2024-02-21 12:48:51 +0100

    Use English on logout route

--- frontend/src/routes/logout.lazy.tsx
+++ frontend/src/routes/logout.lazy.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react'
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { Link, createLazyFileRoute } from '@tanstack/react-router'
 import { useMutation } from '@tanstack/react-query'
 import { AlertCircle, Loader2 } from 'lucide-react'
 
 import { logoutMutationOptions } from '@/lib/queries'
-import { Headline1 } from '@/components/ui/typography'
+import { Headline1, Large } from '@/components/ui/typography'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
 
@@ -22,16 +22,27 @@ function Logout() {
   useEffect(() => logout(), [logout])
 
   return (
-    <div className="mx-auto mt-8 max-w-md p-2">
+    <div className="mx-auto mt-8 max-w-2xl p-2">
       {isPending && <Loader2 className="mx-auto h-8 w-8 animate-spin" />}
-      {isSuccess && <Headline1>Erfolgreich abgemeldet</Headline1>}
+      {isSuccess && (
+        <>
+          <Headline1>Successfully logged out</Headline1>
+          <Large className="mt-8">
+            You have been successfully logged out.
+            <br />
+            <Link to="/login" className="underline">
+              Log in again.
+            </Link>
+          </Large>
+        </>
+      )}
       {error && !isPending && (
         <Alert variant="destructive" className="[&:not(:first-child)]:mt-8">
           <AlertCircle className="h-4 w-4" />
-          <AlertTitle>Abmeldung fehlgeschlagen</AlertTitle>
+          <AlertTitle>Failed to log out</AlertTitle>
           <AlertDescription>
             <Button variant="destructive" onClick={() => logout()}>
-              Abmeldung erneut versuchen
+              Try again
             </Button>
           </AlertDescription>
         </Alert>
