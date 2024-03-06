b87767509754b2d2328527e69bfa786e1e89cc1e
flomei <contact@florianmeinicke.com>
2024-03-06 10:59:33 +0100

    Update client

--- frontend/src/lib/queries.ts
+++ frontend/src/lib/queries.ts
@@ -21,10 +21,20 @@ export const queryQueryOptions = (queryId: string) =>
 export const patchQueryMutationOptions: MutationOptions<
   z.infer<typeof schemas.QueryOut>,
   Error,
-  { queryId: string } & z.infer<typeof schemas.QueryInUpdate>
+  { queryId: string; discourseme_id: number | undefined }
 > = {
-  mutationFn: ({ queryId, ...body }) =>
-    apiClient.patchQueryId(body, { params: { id: queryId } }),
+  mutationFn: async ({ queryId }) => {
+    // TODO: implement patchQueryMutationOptions
+    console.warn('Query Patch is not implemented yet')
+    const mockResponse: z.infer<typeof schemas.QueryOut> = {
+      id: parseInt(queryId),
+      corpus: {
+        id: 1,
+        name: 'corpus',
+      },
+    }
+    return mockResponse
+  },
   onSuccess: (data) => {
     const queryId = data.id
     if (queryId !== undefined) {
@@ -169,7 +179,7 @@ export const queryConcordancesQueryOptions = (
   })
 
 export const queryConcordancesShuffleMutationOptions: MutationOptions<
-  unknown,
+  { query_id?: number },
   Error,
   string
 > = {
@@ -177,10 +187,11 @@ export const queryConcordancesShuffleMutationOptions: MutationOptions<
     apiClient.postQueryQuery_idconcordanceshuffle(undefined, {
       params: { query_id: queryId },
     }),
-  onSuccess: () => {
-    queryClient.invalidateQueries({
-      queryKey: ['query-concordances'],
-    })
+  onSuccess: (data) => {
+    const queryId =
+      data.query_id === undefined ? undefined : data.query_id.toString()
+    if (queryId === undefined) return
+    queryClient.invalidateQueries(queryConcordancesQueryOptions(queryId))
   },
 }
 
@@ -195,7 +206,7 @@ export const subcorporaQueryOptions = (corpusId: string) =>
   queryOptions({
     queryKey: ['subcorpora', corpusId],
     queryFn: ({ signal }) =>
-      apiClient.getCorpusIdsubcorpora({ params: { id: corpusId }, signal }),
+      apiClient.getCorpusIdsubcorpus({ params: { id: corpusId }, signal }),
   })
 
 export const putSubcorpusMutationOptions: MutationOptions<
