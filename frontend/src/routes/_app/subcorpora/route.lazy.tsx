c5b9a3ecc9d1ba9c0871ffb3c2c730a9eebd3249
flomei <contact@florianmeinicke.com>
2024-07-31 17:52:39 +0200

    Update subcorpus overview

--- frontend/src/routes/_app/subcorpora/route.lazy.tsx
+++ frontend/src/routes/_app/subcorpora/route.lazy.tsx
@@ -1,14 +1,22 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
-import { useMutation, useQuery, useSuspenseQuery } from '@tanstack/react-query'
+import { createLazyFileRoute, Link } from '@tanstack/react-router'
+import { useQuery, useSuspenseQuery } from '@tanstack/react-query'
 import { z } from 'zod'
-import { Loader2, Plus } from 'lucide-react'
+import {
+  ChevronsDownUpIcon,
+  ChevronsUpDownIcon,
+  EyeIcon,
+  Loader2,
+} from 'lucide-react'
 
 import { schemas } from '@/rest-client'
-import { subcorpusById, updateSubcorpus, corpusList } from '@/lib/queries'
+import { subcorpusById, corpusList } from '@/lib/queries'
 import { AppPageFrame } from '@/components/app-page-frame'
 import { Skeleton } from '@/components/ui/skeleton'
-import { Button } from '@/components/ui/button'
 import { ErrorMessage } from '@/components/error-message'
+import { Table, TableCell, TableHeader, TableRow } from '@/components/ui/table'
+import { useState } from 'react'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 export const Route = createLazyFileRoute('/_app/subcorpora')({
   component: Subcorpora,
@@ -26,52 +34,90 @@ function Subcorpora() {
         label: 'New Subcorpus',
       }}
     >
-      {corpora.map((corpus) => (
-        <Corpus corpus={corpus} key={corpus.id} />
-      ))}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Subcorpora #</TableCell>
+              <TableCell />
+            </TableRow>
+          </TableHeader>
+          {corpora.map((corpus) => (
+            <Corpus corpus={corpus} key={corpus.id} />
+          ))}
+        </Table>
+      </div>
     </AppPageFrame>
   )
 }
 
 function Corpus({ corpus }: { corpus: z.infer<typeof schemas.CorpusOut> }) {
+  const [expanded, setExpanded] = useState(false)
+  const toggleExpanded = () => setExpanded((prev) => !prev)
   const corpusId = String(corpus.id ?? '')
   const {
     data: subcorpora,
     isLoading,
     error,
   } = useQuery(subcorpusById(corpusId))
-  const {
-    mutate: newSubcorpus,
-    isPending,
-    error: putError,
-  } = useMutation(updateSubcorpus)
+  const hasSubcorpora = (subcorpora?.length ?? 0) > 0
+
   return (
-    <div className="my-4">
-      {corpus.name}
-      <p>{corpus.description}</p>
-      {isLoading && <Loader2 className="m-2 h-4 w-4 animate-spin" />}
-      {subcorpora?.map((subcorpus) => (
-        <div key={subcorpus.id}>
-          {subcorpus.name}
-          <p>{subcorpus.description}</p>
-        </div>
-      ))}
-      {subcorpora?.length === 0 && (
-        <p className="rounded-md bg-muted px-2 py-1 text-muted-foreground">
-          No subcorpora yet.
-        </p>
-      )}
-      <Button onClick={() => newSubcorpus(corpusId)} disabled={isPending}>
-        {isPending ? (
-          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <Plus className="mr-2 h-4 w-4" />
-        )}
-        New Subcorpus
-      </Button>
-      <ErrorMessage error={putError} />
-      <ErrorMessage error={error} />
-    </div>
+    <>
+      <TableRow className={'my-4'}>
+        <TableCell>{corpus.name}</TableCell>
+        <TableCell>{corpus.description ?? 'n/a'}</TableCell>
+        <TableCell>
+          {isLoading ? (
+            <Loader2 className="m-2 h-4 w-4 animate-spin" />
+          ) : (
+            subcorpora?.length ?? 0
+          )}
+          <ErrorMessage error={error} />
+        </TableCell>
+        <TableCell>
+          {hasSubcorpora && (
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={toggleExpanded}
+              className="-my-3"
+            >
+              {expanded ? (
+                <ChevronsDownUpIcon className="h-4 w-4" />
+              ) : (
+                <ChevronsUpDownIcon className="h-4 w-4" />
+              )}
+            </Button>
+          )}
+        </TableCell>
+      </TableRow>
+      {expanded &&
+        subcorpora?.map((subcorpus) => (
+          <TableRow key={subcorpus.id} className="dashed-b py-0">
+            <TableCell className="py-2 pl-8">{subcorpus.name}</TableCell>
+            <TableCell className="py-2" colSpan={2}>
+              {subcorpus.description}
+            </TableCell>
+            <TableCell className="py-0">
+              <Link
+                className={cn(
+                  buttonVariants({ variant: 'ghost', size: 'icon' }),
+                  '-my-1',
+                )}
+                to="/subcorpora/$subcorpusId"
+                params={{
+                  subcorpusId: String(subcorpus.id),
+                }}
+              >
+                <EyeIcon className="h-4 w-4" />
+              </Link>
+            </TableCell>
+          </TableRow>
+        ))}
+    </>
   )
 }
 
