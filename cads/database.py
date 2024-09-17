f9fdb35287986d05eda6e89a87bffa7e2c10927b
SpitfireX <timm.weber@me.com>
2024-09-17 17:02:56 +0200

    added unique constraints for versioning

--- cads/database.py
+++ cads/database.py
@@ -299,7 +299,8 @@ class Query(db.Model):
     __table_args__ = ({'sqlite_autoincrement': True})
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode(255))
+    name = db.Column(db.Unicode(255))    
+    version = db.Column(db.Unicode(255))   
     type = db.Column(db.Unicode(10))
     modified = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -327,6 +328,10 @@ class Query(db.Model):
 
     wordlist_calls = db.relationship("WordListCall", passive_deletes=True, cascade='all, delete')
     macro_calls = db.relationship("MacroCall", passive_deletes=True, cascade='all, delete')
+
+    __table_args__ = (
+        db.UniqueConstraint("name", "version"), # alternative primary key for version history
+    )
 
     __mapper_args__ = {
         "polymorphic_on": "type",
@@ -836,6 +841,10 @@ class WordList(db.Model):
 
     comment = db.Column(db.Unicode)
 
+    __table_args__ = (
+        db.UniqueConstraint("name", "version", "corpus_id"), # alternative primary key for version history
+    )
+
     @property
     def path(self):
         return os.path.join(current_app.config['CCC_LIB_DIR'], f"corpus_{self.corpus_id}", "wordlists", f"{self.name}__v{self.version}.txt")
@@ -872,10 +881,6 @@ class Macro(db.Model):
 
     __table_args__ = {'sqlite_autoincrement': True}
 
-    # __table_args__ = (
-    #     db.UniqueConstraint('name', 'corpus_id', name='unique_name_corpus'),
-    # )
-
     id = db.Column(db.Integer, primary_key=True)
 
     name = db.Column(db.Unicode(255), nullable=False)
@@ -894,6 +899,10 @@ class Macro(db.Model):
     nested_wordlist = db.relationship("NestedWordList", passive_deletes=True, cascade='all, delete')
     nested_macro = db.relationship("NestedMacro", passive_deletes=True, \
                              cascade='all, delete', primaryjoin="Macro.id == NestedMacro.macro_id")
+
+    __table_args__ = (
+        db.UniqueConstraint("name", "valency", "version", "corpus_id"), # alternative primary key for version history
+    )
 
     def __init__(self, **kwargs):
         super(Macro, self).__init__(**kwargs)
