f304ef7d56243b7b05d10f4be82575419f50cf8d
Philipp Heinrich <philipp.heinrich@fau.de>
2024-10-28 19:55:20 +0100

    R-utils for concordancing and collocation

---
+++ R-utils/collocation.R
@@ -0,0 +1,111 @@
+library(tidyverse)
+library(lubridate)
+library(ggrepel)
+library(httr2)
+library(jsonlite)
+library(magrittr)
+
+#' Align items and scores
+#' 
+#' @param scores scores as provided by cwb-cads (items$scores / items$raw_scores / discoursemes$global_scores)
+#' @param items corresponding items
+#' @returns tibble of items and scores
+items.scores.formatter <- function(scores, items){
+  scores |> 
+    bind_rows(.id = "item") |> 
+    pivot_wider(names_from = measure, values_from = score) |> 
+    mutate(item = items)
+}
+
+#' Align ids and scores
+#' 
+#' @param scores scores as provided by cwb-cads (discoursemes$item_scores, discoursemes$unigram_item_scores)
+#' @param ids discourseme ids
+#' @returns tibble of ids, items, and scores
+discoursemes.scores.formatter <- function(scores, ids){
+  if (length(scores) != length(ids)){
+    stop("length mismatch")
+  }
+  out <- tibble()
+  for (i in 1:length(scores)){
+    loc.items <- scores[[i]] |> extract2("item")
+    loc.scores <- scores[[i]] |> extract2("scores")
+    sc <- items.scores.formatter(loc.scores, loc.items) |> mutate(discourseme_id = ids[i])
+    out <- rbind(out, sc)
+  }
+  return(out)
+}
+
+#' Create table of collocation profile including all discourseme scores
+#' 
+#' @param collocation.items collocation object as returned by cwb-cads
+#' @returns a flat tibble with all scores
+collocation.map <- function(collocation.items){
+  
+  coordinates <- collocation.items |> 
+    extract2("coordinates") |> 
+    distinct()
+  discoursemes.coordinates <- collocation.items |> 
+    extract2("discourseme_coordinates") |> 
+    distinct()
+  
+  items.ids <- collocation.items |> 
+    extract2("items") |>
+    extract2("item")
+  items.scores <- collocation.items |> 
+    extract2("items") |> 
+    extract2("scores")
+  items.raw.scores <- collocation.items |>
+    extract2("items") |>
+    extract2("raw_scores")
+  
+  discoursemes.ids <- collocation.items |> 
+    extract2("discourseme_scores") |> 
+    extract2("discourseme_id")
+  discoursemes.global.scores <- collocation.items |> 
+    extract2("discourseme_scores") |> 
+    extract2("global_scores")
+  
+  discoursemes.items.scores <- collocation.items |> 
+    extract2("discourseme_scores") |> 
+    extract2("item_scores")
+  discoursemes.unigram.items.scores <- collocation.items |> 
+    extract2("discourseme_scores") |> 
+    extract2("unigram_item_scores")
+  
+  # actual data
+  items.scores <- items.scores.formatter(items.scores, items.ids)
+  items.raw.scores <- items.scores.formatter(items.raw.scores, items.ids)
+  discoursemes.global.scores <- items.scores.formatter(discoursemes.global.scores, discoursemes.ids)
+  discoursemes.items.scores <- discoursemes.scores.formatter(discoursemes.items.scores, discoursemes.ids)
+  discoursemes.unigram.items.scores <- discoursemes.scores.formatter(discoursemes.unigram.items.scores, discoursemes.ids)
+  
+  # create maps
+  items.map <- items.scores |> 
+    # filter(! item %in% (discoursemes.unigram.items.scores |> pull(item))) |> 
+    left_join(coordinates, by = "item") |> 
+    mutate(discourseme_id = NA) |> 
+    mutate(source = "items")
+  
+  discoursemes.items.map <- discoursemes.items.scores |> 
+    left_join(coordinates, by = "item", relationship = "many-to-many") |> 
+    mutate(source = "discoursemes_items")
+  
+  discoursemes.unigram.items.map <- discoursemes.unigram.items.scores |> 
+    left_join(coordinates, by = "item", relationship = "many-to-many") |> 
+    mutate(source = "discoursemes_unigram_items")
+  
+  discoursemes.global.map <- discoursemes.global.scores |> 
+    left_join(discoursemes.coordinates, by = "discourseme_id", relationship = "many-to-many") |> 
+    mutate(item = NA, source = "discoursemes")
+  
+  # combine
+  map <- bind_rows(
+    items.map, 
+    discoursemes.global.map,
+    discoursemes.items.map,
+    discoursemes.unigram.items.map
+  )
+  
+  return(map)
+}
---
+++ R-utils/concordance.R
@@ -0,0 +1,133 @@
+library(tidyverse)
+library(lubridate)
+library(ggrepel)
+library(httr2)
+library(jsonlite)
+library(magrittr)
+library(xtable)
+
+
+#' Create LaTeX string with ranges highlighted by (nested) colour boxes
+#' 
+#' @param line the concordance line
+#' @param ranges ranges to be highlighted
+#' @returns a LaTeX string
+latex_line_colours <- function(line, ranges) {
+  
+  # ensure line has correct columns
+  if(!all(c('primary', 'cpos') %in% colnames(line))) {
+    stop("Dataframe must have 'primary' and 'cpos' columns")
+  }
+  
+  # ensure ranges have correct columns
+  if(!all(c('start', 'end', 'colour') %in% colnames(ranges))) {
+    stop("Ranges must have 'start', 'end', and 'colour' columns")
+  }
+  
+  # start building the LaTeX string
+  latex_string <- ""
+  
+  # define function to get all range colours for overlapping ranges
+  get_all_range_colours <- function(pos, ranges) {
+    colours <- c()
+    for (i in 1:nrow(ranges)) {
+      if (pos >= ranges$start[i] && pos <= ranges$end[i]) {
+        colours <- c(colours, ranges$colour[i])
+      }
+    }
+    return(colours)  # Return vector of colours
+  }
+  
+  # iterate through tokens and wrap those in ranges with colour
+  for (i in 1:nrow(line)) {
+
+    token <- line$primary[i]
+    pos <- line$cpos[i]
+    
+    # get the colors for the token's position (all overlapping ranges)
+    colours <- get_all_range_colours(pos, ranges)
+    
+    # wrap token in colour boxes (nested if there are multiple overlapping ranges)
+    if (length(colours) > 0) {
+      for (colour in colours) {
+        token <- paste0("\\colorbox{", colour, "}{", token, "}")
+      }
+    }
+    
+    # append
+    latex_string <- paste0(latex_string, token, " ")
+  }
+  
+  return(latex_string)
+}
+
+#' Create LaTeX string of one concordance line with ranges highlighted by (nested) colour boxes
+#' 
+#' @param concordance concordance object as returned by cwb-cads
+#' @param disc.colours dataframe which maps from discourseme_id to colour
+#' @param number which concordance line to create
+#' @returns line `number` formatted in LaTeX
+latex.line <- function(concordance, disc.colours, number = 1){
+  conc.lines <- concordance |> extract2("lines")
+  conc.line <- conc.lines |> slice(number)
+  disc.ranges <- conc.line |> pull(discourseme_ranges) |> extract2(1) |> left_join(disc.colours, by = "discourseme_id")
+  tokens <- conc.line |> pull(tokens) |> extract2(1)
+  latex_line_colours(tokens, disc.ranges)
+}
+
+#' Create KWIC table of concordance, including meta data (lines$structural)
+#' 
+#' @param concordance concordance object as returned by cwb-cads
+#' @returns concordance as KWIC tibble
+conc.kwic <- function(concordance){
+
+  structural <- concordance |> 
+    extract2("lines") |> 
+    select(starts_with("structural")) |> 
+    extract2("structural")
+  
+  conc.lines <- concordance |> extract2("lines") 
+  conc.lines.df <- tibble(left = c(), node = c(), right = c(), disc_ranges = c())
+  for (i in 1:nrow(conc.lines)){
+    
+    tokens <- conc.lines |> 
+      slice(i) |> 
+      pull(tokens) |> 
+      extract2(1)
+    
+    disc.ranges <- conc.lines |> 
+      slice(i) |> 
+      pull(discourseme_ranges)
+    
+    left <- tokens |> filter(offset < 0) |> pull(primary) |> str_flatten(" ")
+    node <- tokens |> filter(offset == 0) |> pull(primary) |> str_flatten(" ")
+    right <- tokens |> filter(offset > 0) |> pull(primary) |> str_flatten(" ")
+    
+    conc.lines.df <- rbind(
+      conc.lines.df,
+      tibble(left = left, node = node, right = right, disc_ranges = as.character(disc.ranges))
+    )
+  }
+
+  conc.lines.df <- cbind(conc.lines.df, structural) |> tibble()
+  
+  return(conc.lines.df)
+}
+
+#' Create LaTeX table of concordance from KWIC tibble
+#' 
+#' @param conc.tbl output of conc.kwic
+#' @param path.out output path (.tex)
+#' @param crop how many characters to display
+#' @returns 
+latex.lines <- function(conc.tbl, path.out, crop = 40){
+  crop <- max(0, min(crop - max(str_length(conc.tbl$node))))
+  conc.tbl |> 
+    rowwise() |> 
+    mutate(
+      left = str_sub(left, str_length(left) - min(crop, str_length(left)) + 1),
+      right = str_sub(right, end = min(crop, min(crop, str_length(right))))
+    ) |> 
+    xtable() |>
+    print(file = path.out, booktabs = TRUE)
+}
