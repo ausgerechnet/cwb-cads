1e10e9dcffc375efac36a1251ede981439868b95
flomei <contact@florianmeinicke.com>
2024-11-11 15:13:49 +0100

    Update error message component to display response messages if available

--- frontend/shared/components/error-message.tsx
+++ frontend/shared/components/error-message.tsx
@@ -15,11 +15,25 @@ export function ErrorMessage({
   if (error === null) {
     return null
   }
+  // eslint-disable-next-line @typescript-eslint/no-ts-ignore
+  // @ts-ignore - We assume that some errors are thrown via axios; in these cases we check whether the error has a response and display the `message` from there -- if it exists.
+  const responseMessage = error.response?.data?.message
+  const message = responseMessage ?? error.message
   return (
     <Alert variant="destructive" className={className}>
       <AlertCircle className="mr-2 h-4 w-4" />
-      <AlertTitle>{error.name}</AlertTitle>
-      <AlertDescription>{error.message}</AlertDescription>
+      {/*
+         If the error contained a Response with a message, omit the `error.name`.
+         These combinations might be confusing for the user.
+      */}
+      {responseMessage ? (
+        <AlertTitle>{message}</AlertTitle>
+      ) : (
+        <>
+          <AlertTitle>{error.name}</AlertTitle>
+          <AlertDescription>{message}</AlertDescription>
+        </>
+      )}
     </Alert>
   )
 }
