707601973647c05d0bb4512eb9812fd38dd7a7fc
SpitfireX <timm.weber@me.com>
2025-01-30 17:27:14 +0100

    added minimum working flexiconc integration

--- cads/spheroscope/__init__.py
+++ cads/spheroscope/__init__.py
@@ -6,10 +6,11 @@ from apiflask import APIBlueprint
 
 from .. import library
 
-from . import slot_query, query_history
+from . import slot_query, query_history, flexiconc
 
 bp = APIBlueprint('spheroscope', __name__, url_prefix='/spheroscope')
 
 
 bp.register_blueprint(slot_query.bp)
 bp.register_blueprint(query_history.bp)
+bp.register_blueprint(flexiconc.bp)
