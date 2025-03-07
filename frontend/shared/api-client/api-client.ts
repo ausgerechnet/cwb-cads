e82c0cdba282e6ccfe29722948600662df0bc1f1
flomei <contact@florianmeinicke.com>
2025-03-07 12:58:21 +0100

    Fix array parameter serialization

--- frontend/shared/api-client/api-client.ts
+++ frontend/shared/api-client/api-client.ts
@@ -1,7 +1,14 @@
 import { createApiClient } from './__generated__client'
+import qs from 'qs'
 
 export const apiClient = createApiClient(
   // es-lint-disable-next-line
   // @ts-ignore
   import.meta.env.VITE_API_URL || '/api',
+  {
+    axiosConfig: {
+      paramsSerializer: (params) => {
+        return qs.stringify(params, { arrayFormat: 'repeat' })
+    },
+  },
 )
