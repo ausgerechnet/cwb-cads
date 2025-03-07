2ea8ba3e91aaf1b12d87ab9e76242da7a8a57d73
Philipp Heinrich <philipp.heinrich@fau.de>
2025-03-07 16:27:37 +0100

    include tests for additional endpoint

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -1,4 +1,5 @@
 from flask import url_for
+import pytest
 
 
 def test_get_corpus(client, auth):
@@ -87,3 +88,44 @@ def test_create_subcorpus(client, auth):
                                content_type='application/json',
                                headers=auth_header)
         assert subcorpus.status_code == 200
+
+
+@pytest.mark.now
+def test_concordance(client, auth):
+
+    auth_header = auth.login()
+
+    with client:
+
+        client.get("/")
+
+        # discourseme
+        corpora = client.get(url_for('corpus.get_corpora'),
+                             content_type='application/json',
+                             headers=auth_header)
+        assert corpora.status_code == 200
+
+        corpus = client.get(url_for('corpus.get_corpus', id=corpora.json[0]['id']),
+                            content_type='application/json',
+                            headers=auth_header)
+        assert corpus.status_code == 200
+
+        concordance = client.get(url_for('corpus.concordance', id=corpora.json[0]['id']),
+                                 json={
+                                     'corpus_id': corpora.json[0]['id'],
+                                     'items': ['CDU'],
+                                     'p': 'lemma'
+                                 },
+                                 content_type='application/json',
+                                 headers=auth_header)
+        assert concordance.status_code == 200
+
+        concordance = client.get(url_for('corpus.concordance', id=corpora.json[0]['id']),
+                                 json={
+                                     'corpus_id': corpora.json[0]['id'],
+                                     'items': ['CDU'],
+                                     'p': 'lemma'
+                                 },
+                                 content_type='application/json',
+                                 headers=auth_header)
+        assert concordance.status_code == 200
